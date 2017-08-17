faae34d7f05568a6601c69025b27cea26e7de89c
robinwen <dbarobinwen@gmail.com>
2017-08-17 18:34:42 +0800

    [Config] update git push script

--- all_in_one_auto_update_master.sh
+++ all_in_one_auto_update_master.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/zsh
 # Author: Robin Wen
 # Date: 2015-03-10 11:43:27
 # Desc: Auto push after update the repo.
 
 git add -A .
 git commit -m "$1"
-proxychains4 git push origin master
+fuckgfw git push origin master
